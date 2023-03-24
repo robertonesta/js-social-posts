3a9aadb5c54f675706d6f8e87f4bc79c954718de
RobertoNesta <robertonesta1993@gmail.com>
2023-03-24 16:51:12 +0100

    add the function with the markup structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,3 +70,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+console.log(posts)
+
+for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+    //console.log(`id: ${post.id}`)
+    //console.log(`content: ${post.content}`)
+    //console.log(`media: ${post.media}`)
+    //console.log(`name: ${post.name}`)
+    //console.log(`image: ${post.image}`)
+    //console.log(`likes: ${post.likes}`)
+    //console.log(`created: ${post.created}`)
+}
+
+const postEl = document.querySelector(".post-list")
+posts.forEach((post) => {
+    let profilePic = post.author.image;
+    let nameInitials;
+    if (profilePic == null){
+        profilePic = nameInitials;
+    }
+
+    postEl.insertAdjacentElement("beforeend", generatePosts(profilePic)) 
+})
+
+function generatePosts(propic){
+    const markup = `  
+        <!--<div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${propic}" alt="${post.name}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${post.name}</div>
+                            <div class="post-meta__time">${post.created}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${post.content}</div>
+                <div class="post__image">
+                    <img src="${post.media}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div> -->`
+            return markup;
+}
+
